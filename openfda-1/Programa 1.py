21ab32f2bc80fb8c86f984ae30a44e253685154c
ilucas <ismael.cucu@gmail.com>
2018-03-23 17:40:04 +0100

    Priemra parte del ejercicio 1

---
+++ openfda-1/Programa 1.py
@@ -0,0 +1,14 @@
+import http.client
+import json
+
+headers = {'User-Agent': 'http-client'}
+
+conn = http.client.HTTPSConnection("api.fda.gov")
+conn.request("GET", "/drug/label.json", None, headers)
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+repos_raw = r1.read().decode("utf-8")
+conn.close()
+
+repos = json.loads(repos_raw)
+print ("Su id es: ",repos['results'][0]['id'],"usado en: ",repos['results'][0]['purpose'],"fabricado por:",repos['results'][0]['openfda']['manufacturer_name'])
