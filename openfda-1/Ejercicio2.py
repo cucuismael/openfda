358229077e70d1e391a57f767f25d432c4728dfb
Ismael <36306831+cucuismael@users.noreply.github.com>
2018-03-26 21:03:31 +0200

    Add files via upload

--- openfda-1/Ejercicio2.py
+++ openfda-1/Ejercicio2.py
@@ -4,13 +4,14 @@ import json
 headers = {'User-Agent': 'http-client'}
 
 conn = http.client.HTTPSConnection("api.fda.gov")
-conn.request("GET", "/drug/label.json?limit=10", None, headers)
+conn.request("GET", "/drug/label.json?search=_exists_:purpose+AND+_exists_:openfda.manufacturer_name&limit=10", None, headers)
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
 repos_raw = r1.read().decode("utf-8")
 conn.close()
 
 repos = json.loads(repos_raw)
-for i in range(0,9):
+
+for i in range(0,10):
     print ("los datos del medicamento", i,"Son: Su id es: ",repos['results'][i]['id'],"usado en: ",repos['results'][i]['purpose'],"fabricado por:",repos['results'][i]['openfda']['manufacturer_name'])
 #Corregir los nombres de los arrays pues son diferentes para cada numero
