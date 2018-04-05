8ca37e0220aca9fae8a515cea1f1d0c3490441d6
Ismael <36306831+cucuismael@users.noreply.github.com>
2018-04-05 22:02:37 +0200

    Add files via upload

---
+++ servidor.py
@@ -0,0 +1,77 @@
+import http.server
+import socketserver
+import json
+PORT = 8000
+
+class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+
+
+    def do_GET(self):
+        self.send_response(200)
+
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        filename = ""
+        if "api" in self.path:
+            headers = {'User-Agent': 'http-client'}
+
+            conn = http.client.HTTPSConnection("api.fda.gov")
+            conn.request("GET","/drug/label.json?search=_exists_:openfda.generic_name+AND+_exists_:openfda.brand_name&limit=10", None, headers)
+            r1 = conn.getresponse()
+            print(r1.status, r1.reason)
+            repos_raw = r1.read().decode("utf-8")
+            conn.close()
+            repos = json.loads(repos_raw)
+            tag=[]
+            name=[]
+            for i in range(0,10):
+                tag.append(repos['results'][i]['openfda']['brand_name'])
+                name.append(repos['results'][i]['openfda']['generic_name'])
+            archivo="""
+            <!doctype html>
+            <html>
+                <body style='background-color: green' >
+                 <h1>tags: "tag" </h2>
+                 <p>nombres: "nombres"</p>
+                </body>
+            </html>
+            """
+            filename=archivo
+        else:
+            try:
+                if self.path=="/":
+                    filename = "index.html"
+                else:
+                    with open(self.path, "r") as f :
+                        filename = f.read()
+            except:
+                filename="error.html"
+        print("Fichero a servir: {}".format(filename))
+        with open(filename, "r") as f:
+            contenido = f.read()
+
+        message = contenido
+
+        self.wfile.write(bytes(message, "utf8"))
+        print("File served!")
+        return
+
+
+
+Handler = testHTTPRequestHandler
+
+httpd = socketserver.TCPServer(("0.0.0.0", PORT), Handler)
+print("serving at port", PORT)
+
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    print("")
+    print("Interrumpido por el usuario")
+
+print("")
+print("Servidor parado")
+httpd.close()
+
+# https://github.com/joshmaker/simple-python-webserver/blob/master/server.py
