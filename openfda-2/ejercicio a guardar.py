a1e62785236068ca54c8efd815ad7c83ab0896d2
ilucas <ismael.cucu@gmail.com>
2018-04-06 18:00:21 +0200

    Priemra parte del ejercicio 1

---
+++ openfda-2/ejercicio a guardar.py
@@ -0,0 +1,27 @@
+import http.client
+import json
+
+headers = {'User-Agent': 'http-client'}
+
+i=0
+controlador= True
+nombres=[]
+while controlador:
+    conn = http.client.HTTPSConnection("api.fda.gov")
+
+    conn.request("GET", '/drug/label.json?search=openfda.substance_name:"SALICYLIC%20ACID"&skip='+ str(i)+'&limit=100', None, headers)
+    r1 = conn.getresponse()
+    print(r1.status, r1.reason)
+    repos_raw = r1.read().decode("utf-8")
+    conn.close()
+
+    repos = json.loads(repos_raw)
+    for f in range(0,100):
+        i+=1
+        if repos['results'][f]['openfda']['manufacturer_name'] not in nombres:
+            nombres.append(repos['results'][f]['openfda']['manufacturer_name'])
+        if i == int(repos['meta']['results']['total']):
+            controlador= False
+            break
+
+print(nombres)
--- openfda-2/programa 1.py
+++ openfda-2/programa 1.py
@@ -6,22 +6,22 @@ headers = {'User-Agent': 'http-client'}
 i=0
 controlador= True
 nombres=[]
-while controlador:
-    conn = http.client.HTTPSConnection("api.fda.gov")
 
-    conn.request("GET", '/drug/label.json?search=openfda.substance_name:"SALICYLIC ACID"&skip='+ str(i)+'&limit=100', None, headers)
-    r1 = conn.getresponse()
-    print(r1.status, r1.reason)
-    repos_raw = r1.read().decode("utf-8")
-    conn.close()
+conn = http.client.HTTPSConnection("api.fda.gov")
 
-    repos = json.loads(repos_raw)
-    for f in range(0,100):
-        i+=1
-        if repos['results'][f]['openfda']['manufacturer_name'] not in nombres:
-            nombres.append(repos['results'][f]['openfda']['manufacturer_name'])
-        if i == int(repos['meta']['results']['total']):
-            controlador= False
-            break
+conn.request("GET", '/drug/label.json?search=active_ingredient:acetylsalicylic&limit=100', None, headers)
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+repos_raw = r1.read().decode("utf-8")
+conn.close()
+
+repos = json.loads(repos_raw)
+limite= int(repos['meta']['results']['total'])
+print(limite)
+for f in range(0, limite):
+    if repos['results'][i]['openfda']['manufacturer_name'] not in nombres:
+        nombres.append(repos['results'][i]['openfda']['manufacturer_name'])
+
+
 
 print(nombres)
